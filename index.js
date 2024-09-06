5824b945a19c5acef413255901cebe1d7fb1e3ad
LeanCG <leandrocorrea040@gmail.com>
2024-09-05 23:47:01 -0300

    Update: maked integration back and fron in route /

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import indexRoutes from './routes/login.js'
 // Crear una instancia de Express
 const app = express();
 
@@ -20,6 +20,9 @@ app.use((req,res, next) =>{
 })
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); // Servir archivos estáticos desde 'public'
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(cors({
     origin: '*', // Permitir cualquier origen. Cambiar según sea necesario.
 }));
@@ -31,11 +34,12 @@ app.use('/api/users', userRoutes);
 // app.use('/api/likes', likeRoutes);
 // app.use('/api/comments', commentRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/', indexRoutes);
 
 // Servir HTML
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'login.ejs'));
+// });
 
 // Iniciar servidor
 const PORT = 3000;
